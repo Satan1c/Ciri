2ea0504b29c77f87ae7e0d89248e7b6567fcf212
Glitch (hello-express) <none>
2020-04-14 17:41:13 +0000

    ⛳️🚣 Checkpoint ./main.py:4989894/354 ./req.txt:4989894/8

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from discord.ext import commands as c
 b = c.Bot(command_prefix=None)
 
 @b.event
+async def on_ready():
+  print("ready")
+
+@b.event
 async def on_member_join(m):
   e = d.Embed(
     title="",
     description=""
-  )
-  await b.get_channel().send(embed=e)
+  ).set_thumbnail(url=m.avatar_url)
+  
+  await m.add_roles(b.get_guild(542005378049638400).get_role(542012055775870976), reason="new user")
+  await b.get_channel(639709192042709002).send(embed=e)
 
-b.run()
+b.run("Njk5NjczMzUzMDE1MDAxMTE4.XpXzuA.pGlWBQnL839E8uEoV_vV5nnYfLA")
