e4bc1699132941214e30b77d16343cd5476caf7d
Glitch (hello-express) <none>
2020-04-25 13:01:22 +0000

    🎠🍤 Checkpoint ./core/bot.py:4989894/1704 ./cogs/events.py:4989894/1617 ./req.txt:4989894/12 ./glitch.json:4989894/706 ./reqirements.txt:4989894/14

--- cogs/events.py
+++ cogs/events.py
@@ -1,3 +1,6 @@
+import discord as d
+import asyncio as ai
+
 from flask import Flask
 from multiprocessing import Process
 from discord.ext import commands
@@ -9,7 +12,6 @@ def hello():
   return "bumped"
 
 
-    
 class Events(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
@@ -20,6 +22,36 @@ class Events(commands.Cog):
   @commands.listener()
   async def on_ready(self):
     Process(target=self.flask_start).start()
+    print("ready")
+    await self.etime()
+
+  @commands.listener()
+  async def on_member_join(self, m):
+    c = self.get_channel(639709192042709002)
+    fc = self.get_channel(684010692571037706)
+    sc = self.get_channel(542005378049638403)
+    o = self.get_user(348444859360608256)
+    r = self.get_guild(542005378049638400).get_role(542012055775870976)
+
+    e = d.Embed(title="\t**Добро пожаловать**",description=f"\
+      Привет, {m.mention}! Рад видеть тебя в нашем уютном уголке - {m.guild.name}.\n\n\
+      👉 **Обязательно загляни в канал** {fc.mention}!\n\
+      Там ты найдешь всю важную инфу для комфортного времяпровождения на сервере (⌒▽⌒)♡\n\n\
+      💜 **Если хочешь начать общение, просто напиши \"Привет всем\" в канал** {sc.mention}!\n\n\
+      Ты у нас уже - {len(m.guild.members)}-й гость.")
+    
+    e.set_thumbnail(url=m.avatar_url_as(size= 4096, format= None, static_format= "png"))
+    e.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size= 4096, format= None, static_format= "png"))
+
+    await m.add_roles(r, reason="new user")
+    await c.send(embed=e)
+    
+  @commands.listener()
+  async def on_message(self, msg):
+    channel = self.get_channel(702122569851076649).id
+    if msg.channel.id == channel:
+      await msg.add_reaction("<:e_ftyes:701774227610796132>")
+      await msg.add_reaction("<:e_ftno:701774245746704436>")
   
 def setup(bot):
   bot.add_cog(Events(bot))
--- core/bot.py
+++ core/bot.py
@@ -1,5 +1,6 @@
 import discord as d
 import asyncio as ai
+import os
 
 from discord.ext import commands as c
 from discord.ext.tasks import loop as l
@@ -16,6 +17,15 @@ class Ciri(c.Bot):
     self.prefix = "adshgrsfhfhgk,jlij;lj'nhplftujxfvjf"
     self.token = "Njk5NjczMzUzMDE1MDAxMTE4.XpXzuA.pGlWBQnL839E8uEoV_vV5nnYfLA"
     self.etime = self.event_time
+    
+    for file in os.listdir('./cogs'):
+      if file[-3:] == '.py':
+        try:
+          self.load_extension(f'cogs.{file[0:-3]}')
+          print(f'[+] cogs.{file[0:-3]}')
+        except BaseException as err:
+          print(f'[!] cogs.{file[0:-3]} error: `{err}`')
+    print('-' * 30)
     
   def tm(self, string: str = ""):
     hour = str(localtime().tm_hour + 3)
@@ -35,43 +45,6 @@ class Ciri(c.Bot):
       
       await ai.sleep(15)
   
-  
-  
-  async def on_ready(self):
-    print("ready")
-    await self.event_time()
-
-    
-  async def on_member_join(self, m):
-    c = self.get_channel(639709192042709002)
-    fc = self.get_channel(684010692571037706)
-    sc = self.get_channel(542005378049638403)
-    o = self.get_user(348444859360608256)
-    r = self.get_guild(542005378049638400).get_role(542012055775870976)
-
-    e = d.Embed(title="\t**Добро пожаловать**",description=f"\
-      Привет, {m.mention}! Рад видеть тебя в нашем уютном уголке - {m.guild.name}.\n\n\
-      👉 **Обязательно загляни в канал** {fc.mention}!\n\
-      Там ты найдешь всю важную инфу для комфортного времяпровождения на сервере (⌒▽⌒)♡\n\n\
-      💜 **Если хочешь начать общение, просто напиши \"Привет всем\" в канал** {sc.mention}!\n\n\
-      Ты у нас уже - {len(m.guild.members)}-й гость.")
-    
-    e.set_thumbnail(url=m.avatar_url_as(size= 4096, format= None, static_format= "png"))
-    e.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size= 4096, format= None, static_format= "png"))
-
-    await m.add_roles(r, reason="new user")
-    await c.send(embed=e)
-    
-  
-  async def on_message(self, msg):
-    channel = self.get_channel(702122569851076649).id
-    if msg.channel.id == channel:
-      await msg.add_reaction("<:e_ftyes:701774227610796132>")
-      await msg.add_reaction("<:e_ftno:701774245746704436>")
-
-  
-    
-
   def startup(self):
     super().run(self.token)
 
