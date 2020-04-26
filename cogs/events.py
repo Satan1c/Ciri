38e00293bf02a71ebee5ba8870cb02049e94525e
Glitch (hello-express) <none>
2020-04-26 10:02:25 +0000

    🏪🛤 Checkpoint ./cogs/events.py:4989894/280 ./server.py:4989894/199

--- cogs/events.py
+++ cogs/events.py
@@ -1,27 +1,17 @@
 import discord as d
 import asyncio as ai
 
-from flask import Flask
-from multiprocessing import Process
 from discord.ext import commands
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  return "bumped"
 
 
 class Events(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
   
-  def flask_start(self):
-    app.run()
-  
   @commands.listener()
   async def on_ready(self):
-    Process(target=self.flask_start).start()
+    
     print("ready")
     await self.etime()
 
--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 from core.bot import bot
+from flask import Flask
+from multiprocessing import Process
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "bumped"
 
 if __name__ == "__main__":
   bot.startup()
+  Process(target=app.run()).start()
   
