59d991c91343cef726120755715bebcf33235563
Glitch (ciri) <none>
2020-06-05 12:07:12 +0000

    🎢👶 Checkpoint ./cogs/events.py:4989894/6885 ./cogs/test.py:4989894/975

--- cogs/events.py
+++ cogs/events.py
@@ -1,82 +1,83 @@
 import discord as d
-import asyncio as ai
 
 from discord.ext import commands
-import asyncio
-
 
 
 class Events(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-		self.etime = bot.etime
-		self.bump = bot._bump
-	
-	@commands.Cog.listener()
-	async def on_ready(self):
-		print("ready")
-		await self.etime()
+    def __init__(self, bot):
+        self.bot = bot
+        self.etime = bot.etime
+        self.bump = bot.bump
 
-	@commands.Cog.listener()
-	async def on_member_join(self, m):
-		g = self.bot.get_guild(542005378049638400)
-		c = self.bot.get_channel(639709192042709002)
-		fc = self.bot.get_channel(684010692571037706)
-		sc = self.bot.get_channel(542005378049638403)
-		o = self.bot.get_user(348444859360608256)
-		r = g.get_role(542012055775870976)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("ready")
+        await self.etime()
 
-		e = d.Embed(colour=d.Colour.from_rgb(54, 57, 63), title="\t🌞  🇯🇵  ヤーホー !  ( Yaho ! )  🇯🇵  🌞",description=f"\
-			Приветствуем тебя, {m.mention}!\n\n\
-			Только что **Ты** стал частью уютного сервера\n{g.name}!\n\
-			👉 Обязательно загляни в канал {fc.mention}.\n\n\
-			Там ты найдешь всю важную инфу для комфортного времяпровождения на сервере (⌒▽⌒)❤️\n\n\
-			💬 Если хочешь начать общение,\nпросто напиши \"Привет всем\" в канал {sc.mention}!\n\n\
-			Ты у нас уже - {len(m.guild.members)}-й гость.")
-		
-		e.set_thumbnail(url=m.avatar_url_as(size= 4096, format= None, static_format= "png"))
-		e.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size= 4096, format= None, static_format= "png"))
+    @commands.Cog.listener()
+    async def on_member_join(self, m):
+        g = self.bot.get_guild(542005378049638400)
+        c = self.bot.get_channel(639709192042709002)
+        fc = self.bot.get_channel(684010692571037706)
+        sc = self.bot.get_channel(542005378049638403)
+        o = self.bot.get_user(348444859360608256)
+        r = g.get_role(542012055775870976)
 
-		await m.add_roles(r, reason="new user")
-		await c.send(embed=e)
+        e = d.Embed(colour=d.Colour.from_rgb(54, 57, 63), title="\t🌞  🇯🇵  ヤーホー !  ( Yaho ! )  🇯🇵  🌞",
+                    description=f"\
+            Приветствуем тебя, {m.mention}!\n\n\
+            Только что **Ты** стал частью уютного сервера\n{g.name}!\n\
+            👉 Обязательно загляни в канал {fc.mention}.\n\n\
+            Там ты найдешь всю важную инфу для комфортного времяпровождения на сервере (⌒▽⌒)❤️\n\n\
+            💬 Если хочешь начать общение,\nпросто напиши \"Привет всем\" в канал {sc.mention}!\n\n\
+            Ты у нас уже - {len(m.guild.members)}-й гость.")
 
-	@commands.Cog.listener()
-	async def on_member_remove(self, m):
-		g = self.bot.get_guild(542005378049638400)
-		r = g.get_role(542012055775870976)
-		o = self.bot.get_user(348444859360608256)
-		c = self.bot.get_channel(639709192042709002)
+        e.set_thumbnail(url=m.avatar_url_as(size=4096, format=None, static_format="png"))
+        e.set_footer(text=f"{o.name}#{o.discriminator}",
+                     icon_url=o.avatar_url_as(size=4096, format=None, static_format="png"))
 
-		e = d.Embed(title="\t🌛  🇯🇵  さようなら!  ( Sayounara ! )  🇯🇵  🌛",description=f"\
-			Береги себя, {m.mention}!\n\n\
-			Бyдем надеяться, что наш дрyг снова зяглянет к нам! ✨.")
-		
-		e.set_thumbnail(url=m.avatar_url_as(size= 4096, format= None, static_format= "png"))
-		e.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size= 4096, static_format= "png"))
+        await m.add_roles(r, reason="new user")
+        await c.send(embed=e)
 
-		await c.send(embed=e)
+    @commands.Cog.listener()
+    async def on_member_remove(self, m):
+        g = self.bot.get_guild(542005378049638400)
+        r = g.get_role(542012055775870976)
+        o = self.bot.get_user(348444859360608256)
+        c = self.bot.get_channel(639709192042709002)
 
-	@commands.Cog.listener()
-	async def on_message(self, msg):
-		if msg.channel.id == 702122569851076649:
-			await msg.add_reaction("<:e_ftyes:701774227610796132>")
-			await msg.add_reaction("<:e_ftno:701774245746704436>")
-		
-		if msg.channel.id == 684011228531654658 and len(msg.embeds) > 0:
-			if msg.author.id == 315926021457051650 or msg.author.id == 464272403766444044:
-				em = msg.embeds[0]
-				self.bot.get_user(348444859360608256).send(f"{em.title}, {em.description}")
+        e = d.Embed(title="\t🌛  🇯🇵  さようなら!  ( Sayounara ! )  🇯🇵  🌛", description=f"\
+            Береги себя, {m.mention}!\n\n\
+            Бyдем надеяться, что наш дрyг снова зяглянет к нам! ✨.")
 
-		if msg.type == d.MessageType.premium_guild_subscription:
-			em = d.Embed(
-				title=f"{msg.author}\n**Забустил сервер**\n{msg.guild.get_role().mebtion}",
-				description="Огромное спасибо, что помогаете серверу!!")
-			em.set_thumbnail(msg.author.avatar_url_as(static_format='png', size=512))
-      em.set_image(url="https://images.app.goo.gl/YCaWRNtd8qHiziMs8")
-      em.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size= 4096, static_format= "png"))
+        e.set_thumbnail(url=m.avatar_url_as(size=4096, format=None, static_format="png"))
+        e.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size=4096, static_format="png"))
 
-			await msg.guild.get_channel(699990941439754371).send(embed=em)
+        await c.send(embed=e)
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        o = self.bot.get_user(348444859360608256)
+        if msg.channel.id == 702122569851076649:
+            await msg.add_reaction("<:e_ftyes:701774227610796132>")
+            await msg.add_reaction("<:e_ftno:701774245746704436>")
+
+        if msg.channel.id == 684011228531654658 and len(msg.embeds) > 0:
+            if msg.author.id == 315926021457051650 or msg.author.id == 464272403766444044:
+                em = msg.embeds[0]
+                self.bot.get_user(348444859360608256).send(f"{em.title}, {em.description}")
+
+        if msg.type == d.MessageType.premium_guild_subscription:
+            em = d.Embed(
+                title=f"{msg.author}\n**Забустил сервер**\n{msg.guild.get_role(709738102394191984).mebtion}",
+                description="Огромное спасибо, что помогаете серверу!!")
+            em.set_thumbnail(url=msg.author.avatar_url_as(static_format='png', size=512))
+            em.set_image(url="https://images.app.goo.gl/YCaWRNtd8qHiziMs8")
+            em.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size=4096, static_format="png"\
+                                                                                      "Большое спасибо передаёт администрация FT"))
+
+            await msg.guild.get_channel(703587055501967390).send(embed=em)
 
 
 def setup(bot):
-	bot.add_cog(Events(bot))
+    bot.add_cog(Events(bot))
--- cogs/test.py
+++ cogs/test.py
@@ -1,5 +1,6 @@
 import discord as d
 from discord.ext import commands
+import datetime
 
 
 class Test(commands.Cog):
@@ -8,9 +9,18 @@ class Test(commands.Cog):
 
     @commands.command(usage="test")
     @commands.is_owner()
-    async def test(self, ctx, eid: int):
-        emoji = await self.bot.get_guild(542005378049638400).fetch_emoji(eid)
-        await ctx.message.channel.send(emoji.url)
+    async def test(self, ctx):
+        o = ctx.guild.get_member(348444859360608256)
+        em = d.Embed(
+                title=f"{ctx.author}\n**Забустил сервер**\n{msg.guild.get_role(709738102394191984).mebtion}",
+                description="Огромное спасибо, что помогаете серверу!!")
+        em.set_thumbnail(url=ctx.author.avatar_url_as(static_format='png', size=512))
+        em.set_image(url="https://images.app.goo.gl/YCaWRNtd8qHiziMs8")
+        em.timestamp = datetime.datetime.utcnow()
+        em.set_footer(text=f"{o.name}#{o.discriminator}", icon_url=o.avatar_url_as(size=4096, static_format="png"\
+                                                                                      "Большое спасибо передаёт администрация FT"))
+        await ctx.send(embed = em)
+            
 
     @commands.command(usage="react")
     @commands.is_owner()
