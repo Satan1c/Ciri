5cb51f13d96ef26572f4ca62774ddad231adaa30
Alexander <satan1c@ukr.net>
2023-03-23 20:24:11 +0200

    shorten texts in modal

--- Ciri/Models/Modals/Moderation/CinemaRequestModal.cs
+++ Ciri/Models/Modals/Moderation/CinemaRequestModal.cs
@@ -8,14 +8,14 @@ public class CinemaRequestModal : IModal
 	public string Title => "Запрос на просмотр";
 	
 	[InputLabel("Напишите название аниме/фильма")]
-	[ModalTextInput("name", TextInputStyle.Short, "Наименование аниме/фильма в оригинале или на английском, например: Kimi no Na wa.")]
+	[ModalTextInput("name", TextInputStyle.Short, "В оригинале или на английском, например: Kimi no Na wa.")]
 	public string Name { get; set; }
 	
 	[InputLabel("Укажите ссылку на аниме/фильм")]
-	[ModalTextInput("url", TextInputStyle.Short, "Укажите ссылку на аниме/фильм с Shikimori или Wikipedia")]
+	[ModalTextInput("url", TextInputStyle.Short, "Ссылка с Shikimori или Wikipedia")]
 	public string Url { get; set; }
 	
 	[InputLabel("Напишите описание аниме/фильма")]
-	[ModalTextInput("description", TextInputStyle.Paragraph, "Укажите краткое описание аниме/фильма", 1, 3000)]
+	[ModalTextInput("description", TextInputStyle.Paragraph, "Краткое описание аниме/фильма", 1, 3000)]
 	public string Description { get; set; }
 }
